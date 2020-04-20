f277afc14c2706a7a51c043fd72c2043a4018708
Korobov Pavel <korobov.i.am@gmail.com>
2020-04-20 18:10:45 +0300

    Inverse dynamics added
    - Should probably add encoders later

--- curiosity.py
+++ curiosity.py
@@ -5,35 +5,73 @@ from torch.optim import Adam
 
 
 class ForwardModel:
-    def __init__(self, state_dim, action_dim, device, hidden_dim=16):
+    def __init__(self, state_dim, n_actions, device, hidden_dim=16):
 
         layers = []
-        layers.append(nn.Linear(state_dim + action_dim, hidden_dim))
+        layers.append(nn.Linear(state_dim + n_actions, hidden_dim))
         layers.append(nn.ReLU()),
         layers.append(nn.Linear(hidden_dim, hidden_dim)),
         layers.append(nn.ReLU())
         layers.append(nn.Linear(hidden_dim, state_dim))
 
+        self.n_actions = n_actions
         self.net = nn.Sequential(*layers).to(device)
         self.optimizer = Adam(self.net.parameters(), lr=1e-4)
         self.device = device
 
+    # add one hots here
     def intrinsic_reward(self, state, next_state, action):
         state = torch.tensor(state, dtype=torch.float32, device=self.device)
         next_state = torch.tensor(next_state, dtype=torch.float32, device=self.device)
-        action_ohe = torch.zeros(1, 3, device=self.device).scatter_(1, action, 1)
+        action_ohe = torch.zeros(action.shape[0], 3, device=self.device).scatter_(1, action, 1)
 
-        return float(F.mse_loss(self.net(torch.cat([state, action_ohe.squeeze()])), next_state))
+        return float(F.mse_loss(self.net(torch.cat([state, action_ohe.squeeze()], dim=-1)), next_state))
+        # return F.mse_loss(self.net(torch.cat([state, action_ohe.squeeze()])), next_state)
+
+    def loss(self, state, next_state, action):
+        action_ohe = torch.zeros(len(action), self.n_actions, device=self.device).scatter_(1, action.unsqueeze(1), 1)
+        state = torch.tensor(state, dtype=torch.float32, device=self.device)
+        next_state = torch.tensor(next_state, dtype=torch.float32, device=self.device)
+
+        # return float(F.mse_loss(self.net(torch.cat([state, action_ohe.squeeze()])), next_state))
+        return F.mse_loss(self.net(torch.cat([state, action_ohe.squeeze()], dim=-1)), next_state)
 
     def update(self, state, next_state, action):
-        loss = F.mse_loss(self.net(torch.cat([state, action], dim=1)), next_state)
+        loss = self.loss(state, next_state, action)
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
 
 
 class InverseModel:
-    pass
+    def __init__(self, state_dim, n_actions, device, hidden_dim=32):
+
+        layers = []
+        layers.append(nn.Linear(2 * state_dim, hidden_dim))
+        layers.append(nn.ReLU()),
+        layers.append(nn.Linear(hidden_dim, hidden_dim)),
+        layers.append(nn.ReLU())
+        layers.append(nn.Linear(hidden_dim, n_actions))
+        layers.append(nn.Softmax())
+
+        self.net = nn.Sequential(*layers).to(device)
+        self.optimizer = Adam(self.net.parameters(), lr=1e-4)
+        self.device = device
+
+    def intrinsic_reward(self, state, next_state, action):
+        state = torch.tensor(state, dtype=torch.float32, device=self.device).unsqueeze(0)
+        next_state = torch.tensor(next_state, dtype=torch.float32, device=self.device).unsqueeze(0)
+        return float(self.loss(state, next_state, action))
+
+    def loss(self, state, next_state, action):
+        criterion = nn.CrossEntropyLoss()
+        return criterion(self.net(torch.cat([state, next_state], dim=-1)), action.view(-1))
+
+    def update(self, state, next_state, action):
+        loss = self.loss(state, next_state, action)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
 
 
 class ICMModel:
--- experiment.py
+++ experiment.py
@@ -6,7 +6,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 from itertools import count
 from dqn import DQN, ReplayMemory
-from curiosity import ForwardModel
+from curiosity import ForwardModel, InverseModel
 
 import torch
 import torch.nn as nn
@@ -20,24 +20,25 @@ env = gym.envs.make("MountainCar-v0")
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-BATCH_SIZE = 32
+BATCH_SIZE = 128
 GAMMA = 0.99
 EPS_START = 0.9
 EPS_END = 0.01
-EPS_DECAY = 100
+EPS_DECAY = 5000
 TARGET_UPDATE = 10
 
 obs_dim = env.observation_space.shape[0]
 n_actions = env.action_space.n
 
 curiosity = ForwardModel(obs_dim, n_actions, device)
+# curiosity = InverseModel(obs_dim, n_actions, device)
 
 policy_net = DQN(observation_dim=obs_dim, actions_number=n_actions, hidden_size=32).to(device)
 target_net = DQN(observation_dim=obs_dim, actions_number=n_actions, hidden_size=32).to(device)
 target_net.load_state_dict(policy_net.state_dict())
 target_net.eval()
 
-optimizer = optim.Adam(policy_net.parameters(), lr=1e-4)
+optimizer = optim.Adam(policy_net.parameters(), lr=1e-3)
 memory = ReplayMemory(capacity=100000)
 steps_done = 0
 
@@ -55,21 +56,26 @@ def optimize_model():
     # next_state_values = target_net(next_state).max(1)[0].detach()
     expected_state_action_values = reward + next_state_values * GAMMA
 
+    # action_ohe = torch.zeros(len(action), n_actions, device=device).scatter_(1, action.unsqueeze(1), 1)
     loss = F.mse_loss(state_action_values, expected_state_action_values.unsqueeze(1))
 
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
 
-    action_ohe = torch.zeros(len(action), n_actions, device=device).scatter_(1, action.unsqueeze(1), 1)
-    curiosity.update(state, next_state, action_ohe)
+    curiosity.update(state, next_state, action)
+
+    # curiosity.update(state, next_state, action_ohe)
 
 
 num_episodes = 500
 
 cum_reward = 0
+cum_i_reward = 0
 for i_episode in range(num_episodes):
     state = env.reset()
+
+    i_weight = 10
 
     for t in count():
         env.render()
@@ -84,20 +90,23 @@ for i_episode in range(num_episodes):
 
         action_ohe = torch.zeros(len(action), n_actions, device=device).scatter_(1, action, 1)
         i_reward = curiosity.intrinsic_reward(state, next_state, action)
-        memory.add(state, action, next_state, reward * 0.05 + i_reward, 1 - done)
+
+        cum_i_reward += i_reward
+
+        memory.add(state, action, next_state, reward + i_reward * i_weight, 1 - done)
         state = next_state
         optimize_model()
-        # curiosity.update(torch.tensor(state, device=device), torch.tensor(next_state, device=device), torch.tensor(action_ohe, device=device))
-
-        print(reward * 0.05 + i_reward, reward, round(i_reward, 3))
 
         if done:
             episode_durations.append(t + 1)
             break
 
-    if i_episode % 100 == 0 and i_episode > 0:
-        print(f"Reward at episode {i_episode}: {cum_reward / 100}")
-        cum_reward = 0
+    print("__________________________")
+    print(cum_reward, cum_i_reward * i_weight)
+    print("__________________________")
+
+    cum_reward = 0
+    cum_i_reward = 0
 
     if i_episode % TARGET_UPDATE == 0:
         target_net.load_state_dict(policy_net.state_dict())
